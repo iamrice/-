acd5466a9a8d78cb08f4b9315d64e809d7dcf794
WVVdragon <925168472@qq.com>
2021-06-23 22:18:51 +0800

    sync_to_target_db.py

--- sync_to_target_db.py
+++ sync_to_target_db.py
@@ -1,11 +1,3 @@
-"""
-1. 一个字典：{type:'', update_items:[],update_content:{}}，字典中的两个成员分别对应 TODO 中的两部分
-			e.g.{type:'update',update_items:['001','008'],update_content:{'course':'math','time':'monday'}} 注：course和time应当是目标端的属性名，不是源端的。
-			e.g.{type:'insert',update_items:[],update_content:{'course_id':'009','course_start_time':'2020-09-01'}}
-			e.g.{type:'delete',update_items:['001','008'],update_content:{}}
-
-			update_content['start_time'].strftime('%Y-%m-%d')
-"""
 def sync_to_target_db(update_unit, target_db):
     
     updateItem = []
@@ -20,20 +12,16 @@ def sync_to_target_db(update_unit, target_db):
                 target_db.pgsUpdate(j,(cond,i))
 
     elif update_unit['type'] == 'insert':
-        updateItem = update_unit['update_items']
+        updateContent = update_unit['update_content']
+        print(updateContent)
         paramsTemp = []
-        for i in updateItem:
-            value = ""
-            if i == "course_start_time" or i == "course_end_time": 
-                value = updateItem[i].strftime('%Y-%m-%d')
-            else:
-                value = updateItem[i]
-            paramsTemp.append(value)
+        for i in updateContent:
+            paramsTemp.append(updateContent[i])
         params = tuple(paramsTemp)
         target_db.pgsInsert(params)
 
-    else update_unit['type'] == 'delete':
-        updateContent = update_unit['update_content']
-        for i in updateContent:
+    else:
+        updateItems = update_unit['update_items']
+        for i in updateItems:
+            #print(i)
             target_db.pgsDelete(i)
-
